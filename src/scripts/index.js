d776cb2fd202c5355e60c2bba80fb7dbcfb5600d
falihnurandino <andinofalih1@gmail.com>
2022-06-06 22:21:24 +0700

    implement renderPage

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -2,3 +2,21 @@ import 'regenerator-runtime';
 import '../styles/style.css';
 import '../styles/responsive.css';
 import '../styles/contact.css';
+import App from './views/app';
+
+const app = new App({
+  button: document.querySelector('.navbar-toggler-icon'),
+  drawer: document.querySelector('#menu'),
+  content: document.querySelector('#mainContent'),
+});
+
+window.addEventListener('hashchange', () => {
+    app.renderPage();
+  });
+
+  window.addEventListener('load', () => {
+    app.renderPage();
+  });
+
+  
+// import '../styles/contact.css';
