42f2415c81b65757eb82388565b574ee8072b0a4
yuda <yohanesbagus56@gmail.com>
2022-06-16 21:44:54 +0700

    Add tabindex="0"

--- src/scripts/views/pages/contact.js
+++ src/scripts/views/pages/contact.js
@@ -3,13 +3,13 @@ const Contact = {
         return `
         <section class="form-head">
         <div class="title-contact container-fluid">
-            <h1>we'd like to hear from you</h1>
+            <h1 tabindex="0">we'd like to hear from you</h1>
         </div>
     </section>
     <section class="wrap">
         <div class="form-wrap">
             <div>
-                <p class="lead">Whether you're curious about services or ordered-we're ready to answer any and all
+                <p tabindex="0" class="lead">Whether you're curious about services or ordered-we're ready to answer any and all
                     questions.</p>
             </div>
             <div>
@@ -18,7 +18,7 @@ const Contact = {
                     <input name="email" type="email" class="form-control" placeholder="Email" requied><br>
                     <input name="subject" type="text" class="form-control" placeholder="Subject" requied><br>
                     <textarea name="message" class="form-control" placeholder="Message" rows="3" requied></textarea><br>
-                    <button type="submit" class="form-control-button">Send Message</button>
+                    <button tabindex="0" type="submit" class="form-control-button">Send Message</button>
                 </form>
             </div>
         </div>
