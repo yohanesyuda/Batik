eecb773774b95d336b0ff2352ac960161c70d8b7
yuda <yohanesbagus56@gmail.com>
2022-06-16 21:42:12 +0700

    Add tabindex="0"

--- src/scripts/views/pages/home.js
+++ src/scripts/views/pages/home.js
@@ -1,37 +1,35 @@
 const Home = {
     async render() {
         return `
+        
         <section class=" hero_container">
             <h1 tabindex="0" class="hero-title">comfy with indonesian vibes</h1>
         </section>
-
+        
         <section class="explore_content" id="main_content">
-            <h2>Our Service</h2>
+            <h2 tabindex="0">Our Service</h2>
 
             <div class="grid-service">
-                <div class="g-col-6 g-col-md-4">
-                <picture>
+                <div tabindex="0" class="g-col-6 g-col-md-4">
+                <picture tabindex="0">
                     <source type="image/jpeg" srcset="./image/img3-small.jpg">
                     <img class="list-item-image" src="./image/img3-large.jpg" alt="jumbotron" />
                 </picture>
-                    <p class="title_service">
-                        Retail
-                    </p>
-                    <p class="description">
+                    <p tabindex="0" class="title_service">Retail</p>
+                    <p tabindex="0" class="description">
                         Kami memiliki sejarah yang panjang dan pengalaman dalam memproduksi
                         batik berkualitas tinggi
                         untuk segala acara. Beberapa merk yang paling populer ada di e
                         commerce kesayangan anda
                     </p>
                 </div>
-                <div class="g-col-6 g-col-md-4">
-                <picture>
-                    
+                <div tabindex="0" class="g-col-6 g-col-md-4">
+                <picture tabindex="0">    
                 <source type="image/jpeg" srcset="./image/img2-small.jpg">
                 <img class="list-item-image" src="./image/img2-large.jpg" alt="jumbotron" />
                 </picture>
-                    <p class="title_service">pesanan khusus</p>
-                    <p class="description">Melayani pemesanan khusus, baik pesanan pribadi
+                    <p tabindex="0" class="title_service">pesanan khusus</p>
+                    <p tabindex="0" class="description">Melayani pemesanan khusus, baik pesanan pribadi
                         dalam jumlah yang kecil
                         maupun
                         besar berupa
@@ -41,14 +39,13 @@ const Home = {
                         mutakhir akan membantu klien kami untuk dapat merealisasikan visi
                         mereka.</p>
                 </div>
-                <div class="g-col-6 g-col-md-4">
-                <picture>
-                    
+                <div tabindex="0" class="g-col-6 g-col-md-4">
+                <picture tabindex="0">
                 <source type="image/jpeg" srcset="./image/img4-small.jpg">
                 <img class="list-item-image" src="./image/img4-large.jpg" alt="jumbotron" />
-            </picture>
-                    <p class="title_service">Expor</p>
-                    <p class="description">Memiliki bisnis ekspor yang bertumbuh pesat.
+                </picture>
+                    <p tabindex="0" class="title_service">Expor</p>
+                    <p tabindex="0" class="description">Memiliki bisnis ekspor yang bertumbuh pesat.
                         Meski Eropa adalah tujuan
                         utamanya,
                         kami juga melayani
@@ -59,93 +56,93 @@ const Home = {
         </section>
 
         <section class="our_collection" id="product">
-            <h3>Our New Collection</h3>
+            <h3 tabindex="0">Our New Collection</h3>
 
             <div class="grid-product">
-                <div class="list-product g-col-6 g-col-md-4">
-                <picture>
-                <source type="image/webp" srcset="./image/product1.webp">
+                <div tabindex="0" class="list-product g-col-6 g-col-md-4">
+                <picture tabindex="0">
+                    <source type="image/webp" srcset="./image/product1.webp">
                     <source type="image/jpeg" srcset="./image/product1-small.jpg">
                     <img class="product-image" src="./image/product1-large.jpg" alt="product batik">
                 </picture>
-                    <p class="title_product"> DB Peksi Mandasari Merah</p>
+                    <p tabindex="0" class="title_product"> DB Peksi Mandasari Merah</p>
                 </div>
-                <div class="list-product g-col-6 g-col-md-4">
-                <picture>
-                <source type="image/webp" srcset="./image/product2.webp">
+                <div tabindex="0" class="list-product g-col-6 g-col-md-4">
+                <picture tabindex="0">
+                    <source type="image/webp" srcset="./image/product2.webp">
                     <source type="image/jpeg" srcset="./image/product2-small.jpg">
                     <img class="product-image" src="./image/product2-large.jpg" alt="product batik">
                 </picture>
-                    <p class="title_product"> Visc Jajaran Sulasih Sogan</p>
+                    <p tabindex="0" class="title_product"> Visc Jajaran Sulasih Sogan</p>
                 </div>
-                <div class="list-product g-col-6 g-col-md-4">
-                <picture>
-                <source type="image/webp" srcset="./image/product3.webp">
-                <source type="image/jpeg" srcset="./image/product3-small.jpg">
-                <img class="product-image" src="./image/product3-large.jpg" alt="product batik">
-            </picture>
-                    <p class="title_product"> Visc Jajaran Sulasih Sogan Coklat</p>
+                <div tabindex="0" class="list-product g-col-6 g-col-md-4">
+                <picture tabindex="0">
+                    <source type="image/webp" srcset="./image/product3.webp">
+                    <source type="image/jpeg" srcset="./image/product3-small.jpg">
+                    <img class="product-image" src="./image/product3-large.jpg" alt="product batik">
+                </picture>
+                    <p tabindex="0" class="title_product"> Visc Jajaran Sulasih Sogan Coklat</p>
                 </div>
-                <div class="list-product g-col-6 g-col-md-4">
-                <picture>
-                <source type="image/webp" srcset="./image/product4.webp">
-                <source type="image/jpeg" srcset="./image/product4-small.jpg">
-                <img class="product-image" src="./image/product4-large.jpg" alt="product batik">
-            </picture>
-                    <p class="title_product"> PI Soga Klir Sogan Merah</p>
+                <div tabindex="0" class="list-product g-col-6 g-col-md-4">
+                <picture tabindex="0">
+                    <source type="image/webp" srcset="./image/product4.webp">
+                    <source type="image/jpeg" srcset="./image/product4-small.jpg">
+                    <img class="product-image" src="./image/product4-large.jpg" alt="product batik">
+                </picture>
+                    <p tabindex="0" class="title_product"> PI Soga Klir Sogan Merah</p>
                 </div>
-                <div class="list-product g-col-6 g-col-md-4">
-                <picture>
-                <source type="image/webp" srcset="./image/product5.webp">
-                <source type="image/jpeg" srcset="./image/product5-small.jpg">
-                <img class="product-image" src="./image/product5-large.jpg" alt="product batik">
-            </picture>
-                    <p class="title_product"> PA Puger Aji Coklat</p>
+                <div tabindex="0" class="list-product g-col-6 g-col-md-4">
+                <picture tabindex="0">
+                    <source type="image/webp" srcset="./image/product5.webp">
+                    <source type="image/jpeg" srcset="./image/product5-small.jpg">
+                    <img class="product-image" src="./image/product5-large.jpg" alt="product batik">
+                </picture>
+                    <p tabindex="0" class="title_product"> PA Puger Aji Coklat</p>
                 </div>
-                <div class="list-product g-col-6 g-col-md-4">
-                <picture>
-                <source type="image/webp" srcset="./image/product6.webp">
-                <source type="image/jpeg" srcset="./image/product6-small.jpg">
-                <img class="product-image" src="./image/product6-large.jpg" alt="product batik">
-            </picture>
-                    <p class="title_product"> CDK Cenderawasih Kembar Merah</p>
+                <div tabindex="0" class="list-product g-col-6 g-col-md-4">
+                <picture tabindex="0">
+                    <source type="image/webp" srcset="./image/product6.webp">
+                    <source type="image/jpeg" srcset="./image/product6-small.jpg">
+                    <img class="product-image" src="./image/product6-large.jpg" alt="product batik">
+                </picture>
+                    <p tabindex="0" class="title_product"> CDK Cenderawasih Kembar Merah</p>
                 </div>
-                <div class="list-product g-col-6 g-col-md-4">
-                <picture>
-                <source type="image/webp" srcset="./image/product7.webp">
-                <source type="image/jpeg" srcset="./image/product7-small.jpg">
-                <img class="product-image" src="./image/product7-large.jpg" alt="product batik">
-            </picture>
-                    <p class="title_product"> SKM Sekar Manunggal Navy</p>
+                <div tabindex="0" class="list-product g-col-6 g-col-md-4">
+                <picture tabindex="0">
+                    <source type="image/webp" srcset="./image/product7.webp">
+                    <source type="image/jpeg" srcset="./image/product7-small.jpg">
+                    <img class="product-image" src="./image/product7-large.jpg" alt="product batik">
+                </picture>
+                    <p tabindex="0" class="title_product"> SKM Sekar Manunggal Navy</p>
                 </div>
-                <div class="list-product g-col-6 g-col-md-4">
-                <picture>
-                <source type="image/webp" srcset="./image/product8.webp">
-                <source type="image/jpeg" srcset="./image/product8-small.jpg">
-                <img class="product-image" src="./image/product8-large.jpg" alt="product batik">
-            </picture>
-                    <p class="title_product"> CDK Cenderawasih Kembar Hijau</p>
+                <div tabindex="0" class="list-product g-col-6 g-col-md-4">
+                <picture tabindex="0">
+                    <source type="image/webp" srcset="./image/product8.webp">
+                    <source type="image/jpeg" srcset="./image/product8-small.jpg">
+                    <img class="product-image" src="./image/product8-large.jpg" alt="product batik">
+                </picture>
+                    <p tabindex="0" class="title_product"> CDK Cenderawasih Kembar Hijau</p>
                 </div>
             </div>
         </section>
 
         <section class="faqs" id="faqs">
             <div class="batik-faqs">
-                <h4>Rumah Batik FAQs</h4>
+                <h4 tabindex="0">Rumah Batik FAQs</h4>
             </div>
 
             <div class="accordion accordion-flush" id="accordionPanelsStayOpenExample">
                 <div class="accordion-item bg-transparent">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingOne">
-                        <button class="accordion-button bg-transparent " type="button" data-bs-toggle="collapse"
+                    <h2 tabindex="0" class="accordion-header" id="panelsStayOpen-headingOne">
+                        <button tabindex="0" class="accordion-button bg-transparent " type="button" data-bs-toggle="collapse"
                             data-bs-target="#panelsStayOpen-collapseOne" aria-expanded="true"
                             aria-controls="panelsStayOpen-collapseOne">
                             Apa itu Rumah Batik Nusantara ?
                         </button>
                     </h2>
-                    <div id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
+                    <div tabindex="0" id="panelsStayOpen-collapseOne" class="accordion-collapse collapse show"
                         aria-labelledby="panelsStayOpen-headingOne">
-                        <div class="accordion-body ">
+                        <div tabindex="0" class="accordion-body ">
                             Rumah Batik Nusantara merupakan salah satu perusahaan batik di
                             Indonesia yang tetap
                             mempertahankan kualitas dan warisan budaya nusantara Indonesia.
@@ -158,8 +155,8 @@ const Home = {
                 </div>
 
                 <div class="accordion-item bg-transparent">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingTwo">
-                        <button class="accordion-button collapsed bg-transparent " type="button"
+                    <h2 tabindex="0" class="accordion-header" id="panelsStayOpen-headingTwo">
+                        <button tabindex="0" class="accordion-button collapsed bg-transparent " type="button"
                             data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseTwo" aria-expanded="false"
                             aria-controls="panelsStayOpen-collapseTwo">
                             Kenapa saya harus belanja produk Rumah Batik Nusantara ?
@@ -167,7 +164,7 @@ const Home = {
                     </h2>
                     <div id="panelsStayOpen-collapseTwo" class="accordion-collapse collapse"
                         aria-labelledby="panelsStayOpen-headingTwo">
-                        <div class="accordion-body ">
+                        <div tabindex="0" class="accordion-body ">
                             Anda dapat menemukan busana batik dengan desain yang unik,
                             inovatif, dalam tampilan fashion
                             modern. Hal tersebut bisa Anda lihat dari kekayaan dan keindahan
@@ -180,8 +177,8 @@ const Home = {
                 </div>
 
                 <div class="accordion-item bg-transparent">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingThree">
-                        <button class="accordion-button collapsed bg-transparent " type="button"
+                    <h2 tabindex="0" class="accordion-header" id="panelsStayOpen-headingThree">
+                        <button tabindex="0" class="accordion-button collapsed bg-transparent " type="button"
                             data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseThree"
                             aria-expanded="false" aria-controls="panelsStayOpen-collapseThree">
                             Produk apa saja yang dijual di Rumah Batik Nusantara ?
@@ -189,7 +186,7 @@ const Home = {
                     </h2>
                     <div id="panelsStayOpen-collapseThree" class="accordion-collapse collapse"
                         aria-labelledby="panelsStayOpen-headingThree">
-                        <div class="accordion-body">
+                        <div tabindex="0" class="accordion-body">
                             Produk yang ditawarkan oleh umah Batik Nusantara sangatlah
                             beragam mulai dari busana Batik
                             untuk anak-anak, remaja, hingga orang dewasa. Selain itu juga
@@ -200,8 +197,8 @@ const Home = {
                 </div>
 
                 <div class="accordion-item bg-transparent">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingFour">
-                        <button class="accordion-button collapsed bg-transparent " type="button"
+                    <h2 tabindex="0" class="accordion-header" id="panelsStayOpen-headingFour">
+                        <button tabindex="0" class="accordion-button collapsed bg-transparent " type="button"
                             data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFour"
                             aria-expanded="false" aria-controls="panelsStayOpen-collapseFour">
                             Berapa range harga untuk produk yang ditawarkan oleh Rumah Batik
@@ -210,7 +207,7 @@ const Home = {
                     </h2>
                     <div id="panelsStayOpen-collapseFour" class="accordion-collapse collapse"
                         aria-labelledby="panelsStayOpen-headingFour">
-                        <div class="accordion-body">
+                        <div tabindex="0" class="accordion-body">
                             Range harga untuk produk Rumah Batik Nusantara sangatlah beragam
                             sesuai dengan kebutuhan
                             Anda. Harga disesuaikan dengan jenis batik, mulai dari batik
@@ -221,8 +218,8 @@ const Home = {
                 </div>
 
                 <div class="accordion-item bg-transparent">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingFive">
-                        <button class="accordion-button collapsed bg-transparent " type="button"
+                    <h2 tabindex="0" class="accordion-header" id="panelsStayOpen-headingFive">
+                        <button tabindex="0" class="accordion-button collapsed bg-transparent " type="button"
                             data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseFive"
                             aria-expanded="false" aria-controls="panelsStayOpen-collapseFive">
                             Dimana saya bisa mendapatkan produk Rumah Batik Nusantara ?
@@ -230,7 +227,7 @@ const Home = {
                     </h2>
                     <div id="panelsStayOpen-collapseFive" class="accordion-collapse collapse"
                         aria-labelledby="panelsStayOpen-headingFive">
-                        <div class="accordion-body">
+                        <div tabindex="0" class="accordion-body">
                             Produk - produk Rumah Batik Nusantara bisa didapatkan di toko -
                             toko resmi kami yang ada di
                             seluruh indonesia
@@ -239,8 +236,8 @@ const Home = {
                 </div>
 
                 <div class="accordion-item bg-transparent">
-                    <h2 class="accordion-header" id="panelsStayOpen-headingSix">
-                        <button class="accordion-button collapsed bg-transparent " type="button"
+                    <h2 tabindex="0" class="accordion-header" id="panelsStayOpen-headingSix">
+                        <button tabindex="0" class="accordion-button collapsed bg-transparent " type="button"
                             data-bs-toggle="collapse" data-bs-target="#panelsStayOpen-collapseSix" aria-expanded="false"
                             aria-controls="panelsStayOpen-collapseSix">
                             Apakah Rumah Batik Nusantara melakukan penjualan online ?
@@ -248,7 +245,7 @@ const Home = {
                     </h2>
                     <div id="panelsStayOpen-collapseSix" class="accordion-collapse collapse"
                         aria-labelledby="panelsStayOpen-headingSix">
-                        <div class="accordion-body">
+                        <div tabindex="0" class="accordion-body">
                             kami telah melakukan penjualan secara online sejak September
                             2018. Penjualan online Rumah
                             Batik Nusantara melalui : Akun Facebook di @RumahBatikNusantara,
